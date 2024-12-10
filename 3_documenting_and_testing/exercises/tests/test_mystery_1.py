d659326e6d657aca6ab881dc1774cc74aa0f652a
WuorBhang <uhuribhang211@gmail.com>
2024-12-10 21:00:05 +0300

    writing the test of the documentations of mystery_1 files in test_mystery_1 files

--- 3_documenting_and_testing/exercises/tests/test_mystery_1.py
+++ 3_documenting_and_testing/exercises/tests/test_mystery_1.py
@@ -1,5 +1,36 @@
 import unittest
-
+from ..mystery_1 import mystery_1
 
 class TestMystery1(unittest.TestCase):
-    """ """
+    def test_1(self):
+        """It should correctly sort a list of integers."""
+        actual = mystery_1([5, 3, 8, 6, 2])
+        expected = [2, 3, 5, 6, 8]
+        self.assertEqual(actual, expected)
+
+    def test_2(self):
+        """It should return the same list if it is already sorted."""
+        actual = mystery_1([1, 2, 3, 4, 5])
+        expected = [1, 2, 3, 4, 5]
+        self.assertEqual(actual, expected)
+
+    def test_3(self):
+        """It should correctly sort a list with negative integers."""
+        actual = mystery_1([10, -2, 4, 3])
+        expected = [-2, 3, 4, 10]
+        self.assertEqual(actual, expected)
+
+    def test_4(self):
+        """It should return an empty list if the input is empty."""
+        actual = mystery_1([])
+        expected = []
+        self.assertEqual(actual, expected)
+
+    def test_5(self):
+        """It should return the same list if it contains a single element."""
+        actual = mystery_1([9])
+        expected = [9]
+        self.assertEqual(actual, expected)
+
+if __name__ == "__main__":
+    unittest.main()
