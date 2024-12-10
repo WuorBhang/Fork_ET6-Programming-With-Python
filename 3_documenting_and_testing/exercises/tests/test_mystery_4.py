9dd1c35ab971e871e775ef4192cf0dff68753d93
WuorBhang <uhuribhang211@gmail.com>
2024-12-10 21:01:43 +0300

    writing the test of the documentations of mystery_4 files in test_mystery_4 files

--- 3_documenting_and_testing/exercises/tests/test_mystery_4.py
+++ 3_documenting_and_testing/exercises/tests/test_mystery_4.py
@@ -1,5 +1,34 @@
 import unittest
+from ..mystery_4 import mystery_4
 
 
 class TestMystery4(unittest.TestCase):
-    """ """
+    def test_1(self):
+        """It should return all instances of the integer 2."""
+        actual = mystery_4([1, 2, 3, 4, 2, 5], 2)
+        expected = [2, 2]
+        self.assertEqual(actual, expected)
+
+    def test_2(self):
+        """It should return an empty list if no elements match."""
+        actual = mystery_4([10, 20, 30, 40], 15)
+        expected = []
+        self.assertEqual(actual, expected)
+
+    def test_3(self):
+        """It should return all elements if they all match the integer 5."""
+        actual = mystery_4([5, 5, 5, 5], 5)
+        expected = [5, 5, 5, 5]
+        self.assertEqual(actual, expected)
+
+    def test_4(self):
+        """It should return an empty list if the input list is empty."""
+        actual = mystery_4([], 1)
+        expected = []
+        self.assertEqual(actual, expected)
+
+    def test_5(self):
+        """It should return an empty list if `b` is not in the list."""
+        actual = mystery_4([1, 2, 3, 4, 5], 6)
+        expected = []
+        self.assertEqual(actual, expected)
