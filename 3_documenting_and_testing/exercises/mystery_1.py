8574cd6879e2b7de9a3d1c2beaf155e2517f09e0
WuorBhang <uhuribhang211@gmail.com>
2024-12-10 20:56:21 +0300

    updating the documentations of mystery_1 files

--- 3_documenting_and_testing/exercises/mystery_1.py
+++ 3_documenting_and_testing/exercises/mystery_1.py
@@ -1,4 +1,29 @@
 def mystery_1(a):
+    """
+    Sorts a list of integers in ascending order using the Bubble Sort algorithm.
+
+    Parameters:
+    a (list): A list of integers to be sorted.
+
+    Returns:
+    list: The input list sorted in ascending order.
+
+    Examples:
+    >>> mystery_1([5, 3, 8, 6, 2])
+    [2, 3, 5, 6, 8]
+
+    >>> mystery_1([1, 2, 3, 4, 5])
+    [1, 2, 3, 4, 5]
+
+    >>> mystery_1([10, -2, 4, 3])
+    [-2, 3, 4, 10]
+
+    >>> mystery_1([])
+    []
+
+    >>> mystery_1([9])
+    [9]
+    """
     b = len(a)
     for c in range(b):
         for d in range(0, b - c - 1):
