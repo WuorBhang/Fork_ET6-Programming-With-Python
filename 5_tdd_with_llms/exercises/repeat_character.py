c44fbf6dcaf7ef19b2ebb885aa0a74d2fde2728f
WuorBhang <uhuribhang211@gmail.com>
2024-12-12 11:48:21 +0300

    adding documenatations to repeat_character.py files

--- 5_tdd_with_llms/exercises/repeat_character.py
+++ 5_tdd_with_llms/exercises/repeat_character.py
@@ -6,5 +6,22 @@ The function returns a string with each occurrence of the character repeated n t
 """
 
 
-def repeat_character():
-    pass
+def repeat_character(string, char, n):
+    """_summary_
+    This function will takes in a string, a single character, and a number
+    and returns a string with each appearance
+    of the character repeated n times.
+
+    Args:
+        string (str):The input string to be processed.
+        char (char):The character to be repeated.
+        n (int): The number of times the character should be repeated. It is an integer.
+
+    Returns:
+        str: The input string with each appearance of the character repeated n times.
+    """
+
+    if n == 0:
+        return string
+    else:
+        return string.replace(char, char * n)
